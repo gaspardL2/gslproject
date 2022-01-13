50795628efb8eeb32b6e5ff3c80865f5547c60e2
gaspardL2 <gaspard.tourk@tutanota.com>
2022-01-13 09:16:02 +0100

    added new branch "gsl_functions and created new header/source file

---
+++ bessel_functions.c
@@ -0,0 +1,8 @@
+//
+// Created by hawking on 1/11/22.
+//
+#include "bessel_functions.h"
+#include <gsl/gsl_sf_bessel.h>
+
+
+
---
+++ bessel_functions.h
@@ -0,0 +1,8 @@
+//
+// Created by hawking on 1/11/22.
+//
+
+#ifndef GSLPROJECT_BESSEL_FUNCTIONS_H
+#define GSLPROJECT_BESSEL_FUNCTIONS_H
+void besselfunctions();
+#endif //GSLPROJECT_BESSEL_FUNCTIONS_H
